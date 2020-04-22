3f98980790dbea7343912922056eac89552c37a8
Aldy <muhamadaldy17@gmail.com>
2020-04-22 19:41:38 +0700

    2020/04/22
    - update all GUI to resize any resolution (in canvas use "scale with screen size")
    - fix all GUI position and size

---
+++ Assets/Scripts/Multi-dimen Script/MDLoginScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MDLoginScript : MonoBehaviour
+{
+
+    public Text welcomeText, loginText;
+    public InputField emailInput, passwordInput;
+    public Button loginButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnEnable() {
+        Debug.Log(welcomeText.text);
+        Debug.Log(emailInput.text);
+        Debug.Log(emailInput.placeholder);
+        Debug.Log(passwordInput.text);
+        Debug.Log(passwordInput.placeholder);
+        Debug.Log(loginText.text);
+    }
+}
